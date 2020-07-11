9f15568e1671a5fa19f78b972a2133c574eb3e58
Dandy <dandycvc@gmail.com>
2020-07-11 12:30:16 +0300

    Initial commit

---
+++ L01_HW1.py
@@ -0,0 +1,4 @@
+user_name = input("What is your name?\n")
+user_age = input("How old are you?\n")
+user_location = input("Where do you live?\n")
+print("Hello, %s!\nYour age is %s\nYou live in %s" %(user_name, user_age, user_location))
