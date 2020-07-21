8532ef2029e96833944df54a48ba2bf020e78500
Dandy <dandycvc@gmail.com>
2020-07-21 17:56:01 +0300

    HW5

--- L05_HW6.py
+++ L05_HW6.py
@@ -1,10 +1,12 @@
 user_number = 1
 positive_counter = 0
-total_counter = 0
+negative_counter = 0
 while user_number != 0:
     user_number = int(input("Enter integer number: "))
     if user_number > 0:
         positive_counter += 1
-    total_counter += 1
-print("Total quantity of numbers = %d\nPositive numbers %.2f\n \
-Negative numbers %.2f" %(total_counter, positive_counter * 100 /))
+    elif user_number < 0:
+        negative_counter += 1
+total_counter = positive_counter + negative_counter
+print("Total numbers count = %d\nPositive numbers %.2f%%\nNegative numbers %.2f%%"
+      % (total_counter, positive_counter * 100 / total_counter, negative_counter * 100 / total_counter))
