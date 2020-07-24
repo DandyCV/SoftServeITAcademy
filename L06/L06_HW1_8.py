d661875c69b8c0194d7adb1204d84ff764739cac
Dandy <dandycvc@gmail.com>
2020-07-24 22:49:50 +0300

    HW6

--- L06/L06_HW1_8.py
+++ L06/L06_HW1_8.py
@@ -5,18 +5,11 @@ for number in range(10):
     random_list.append(randint(-100, 100))
 print("Random list 1:", random_list)
 
-'''Я помітив що, якщо мінімальний елемент знаходиться лівіше максимального то заміна не відбувається, оскільки
-в момент пошуку куди запихнути найменьший елемент з temp'у, заново обчислюється індекс найбільшого. 
-А в цей момент ми маємо в списку 2 однакових найбільших елементи і відповідно повертається індекс лівішого з них. 
-А за цим індексом до цього був найменший елемент. І тоді ми найменший елемент з temp'у засовуємо в той самий індекс
-(тобіто нічого не змінюється). Для цього я перевіряю який елемент лівіше, найбільший чи найменший і відповідно в temp
-присвоюю той, що правіше. Таск з лекції також працює наполовину =)'''
-if random_list.index(min(random_list)) > random_list.index(max(random_list)):
-    temp = random_list[random_list.index(min(random_list))]
-    random_list[random_list.index(min(random_list))] = random_list[random_list.index(max(random_list))]
-    random_list[random_list.index(max(random_list))] = temp
-else:
-    temp = random_list[random_list.index(max(random_list))]
-    random_list[random_list.index(max(random_list))] = random_list[random_list.index(min(random_list))]
-    random_list[random_list.index(min(random_list))] = temp
+index_max = random_list.index(max(random_list))
+index_min = random_list.index(min(random_list))
+
+temp = random_list[index_min]
+random_list[index_min] = random_list[index_max]
+random_list[index_max] = temp
+
 print("Random list 2:", random_list)
